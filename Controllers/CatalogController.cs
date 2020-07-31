8e7cc830883478483a5383a891e41b06b9ae603d
shayc20 <61070630+shayc20@users.noreply.github.com>
2020-07-31 09:58:00 -0700

    Add files via upload

---
+++ Controllers/CatalogController.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace PropertyRental.Controllers
+{
+    public class CatalogController : Controller{
+
+
+        public IActionResult Test(){
+            return View();
+        }
+
+        public IActionResult Index(){
+            return View();
+        }
+
+         public IActionResult Register(){
+            return View();
+        }
+
+
+    }
+}
